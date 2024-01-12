1cdc87cb7f2cc31291c8e0bbb4f07f8984f16f8d
Omar Alejandro Lozada <alejandro01lozada@gmail.com>
2024-01-11 21:43:33 -0600

    listocomponentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 import { VerProyectoComponent } from './componentes/ver-proyecto/ver-proyecto.component';
+import { ActividadComponent } from './componentes/actividad/actividad.component';
+import { SubactComponent } from './componentes/subact/subact.component';
 
 
 const routes: Routes = [
   {path: '',redirectTo:'dashboard', pathMatch: 'full'},
   {path: 'dashboard',component:DashboardComponent},
   {path: 'ver-proyecto/:id',component:VerProyectoComponent},
+  {path:'actividad',component:ActividadComponent},
+  {path:'subact',component:SubactComponent}
 ];
 
 @NgModule({
